f9ad1fbf27dd5be21adf4df97d3cfdb8826924f4
Guikingone <guillaume.loulier@hotmail.fr>
2016-09-03 09:15:42 +0200

    [+][!M] || First component|Done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,15 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template("AppBundle/Index/index.html.twig")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
-    }
+    public function indexAction(){}
 }
---
+++ web/dev/js/Polymer/Symfony-elements.js
@@ -0,0 +1,6 @@
+Polymer({
+    is: "symfony-elements",
+    ready: function() {
+        this.textContent = "Bonjour depuis Symfony et Polymer !"
+    }
+});
